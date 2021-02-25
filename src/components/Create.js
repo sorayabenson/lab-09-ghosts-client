ab0ed8da814d43a168b8c68c4dc9aced38f78477
Soraya Benson <soraya.benson@gmail.com>
2021-02-24 23:09:31 -0800

    form on create page works and redirects to home on submit

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,23 +1,84 @@
 import React, { Component } from 'react';
 import './Create.css';
+import { makeGhost } from './apiUtils.js';
 
 export default class Create extends Component {
+    state ={
+        name: '',
+        img: '',
+        description: '',
+        category_id: 1,
+        price: 0,
+        price_currency: '',
+        trustworthy: true,
+        owner_id: 1,
+    }
+
+    handleName = (e) => {
+        this.setState({ name: e.target.value });
+    }
+    
+
+    handleImg = (e) => {
+        this.setState({ img: e.target.value });
+    }
+
+    handleDescription = (e) => {
+        this.setState({ description: e.target.value });
+    }
+
+    handleCategory = (e) => {
+        this.setState({ category_id: Number(e.target.value) });
+    }
+
+    handlePrice = (e) => {
+        this.setState({ price: Number(e.target.value) });
+    }
+
+    handleCurrency = (e) => {
+        this.setState({ price_currency: e.target.value });
+    }
+
+    handleTrust = (e) => {
+        this.setState({ trustworthy: e.target.value });
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await makeGhost(this.state);
+
+        this.props.history.push('/ghosts');
+        console.log(this.state);
+        window.location.pathname = '/';
+    }
+
     render() {
         return (
             <div className="main">
                 <h3>make a new ghost</h3>
-                <form>
-                    <input type="text" placeholder="ghost name" className="nameInput" />
-                    <input type="url" placeholder="image url" className="urlInput" />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="ghost name" className="nameInput"
+                    value={this.state.name} 
+                    onChange={this.handleName} />
+                    <input type="url" placeholder="image url" className="urlInput" 
+                    value={this.state.img} 
+                    onChange={this.handleImg} />
 
                     <textarea 
                     wrap="soft"
                     placeholder="ghost description" 
-                    className="descriptionInput" />
+                    className="descriptionInput"
+                    value={this.state.description} 
+                    onChange={this.handleDescription} />
 
                     <div className="priceWrapper">
-                        <input type="number" placeholder="price" className="priceInput" />
-                        <input type="text" placeholder="ghost currency" className="currencyInput" />
+                        <input type="number" placeholder="price" className="priceInput"
+                        value={this.state.price} 
+                        onChange={this.handlePrice} />
+                        <input type="text" placeholder="ghost currency" className="currencyInput"
+                        value={this.state.price_currency} 
+                        onChange={this.handleCurrency} />
                     </div>
 
                     <div className="trustworthyWrapper">
@@ -27,21 +88,25 @@ export default class Create extends Component {
                         type="radio" 
                         value="true" 
                         id="true"
-                        name="trustworthy"/>
-                        <label for="true">yes</label>
+                        name="trustworthy"
+                        onChange={this.handleTrust}/>
+                        <label htmlFor="true">yes</label>
                         
                         <input
                         type="radio" 
                         value="false" 
                         id="false"
-                        name="trustworthy"/>
-                        <label for="false">no</label>
+                        name="trustworthy"
+                        onChange={this.handleTrust}/>
+                        <label htmlFor="false">no</label>
 
                     </div>
 
                     <div className="categoryWrapper">
                         <h6>category:</h6>
-                        <select>
+                        <select
+                        value={this.state.category_id} 
+                        onChange={this.handleCategory}>
                             <option value="1">skill</option>
                             <option value="2">food</option>
                             <option value="3">care</option>
@@ -50,7 +115,8 @@ export default class Create extends Component {
 
                 </form>
 
-                <button>create!</button>
+                <button
+                onClick={this.handleSubmit}>create!</button>
             </div>
         )
     }
